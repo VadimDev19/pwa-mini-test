be925bc10a8f5438f162a2f32aec35ec07788148
VadimDev19 <vadimtkacenko066@gmail.com>
2022-07-28 20:51:59 +0300

    fix4

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+//@ts-nocheck
+import React, { useEffect, useState, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("app");
-  window.addEventListener("beforeinstallprompt", (ev) => {
-    console.log("beforeinstallprompt app", ev);
-  });
+  const [isShowBtn, setIsShowBtn] = useState<boolean>(true);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const deferredInstall = useRef<Event | null>(null);
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("beforeinstallprompt", (event) => {
+      deferredInstall.current = event;
+      console.log("beforeinstallprompt app", event);
+    });
+  }, []);
+
+  const handleButtonClick = () => {
+    setIsShowBtn(false);
+    if (deferredInstall.current) {
+      console.log("evt handleButtonClick", deferredInstall.current);
+      deferredInstall.current.prompt();
+      deferredInstall.current.userChoice.then((choice) => {
+        if (choice.outcome === "accepted") {
+          console.log("installed");
+        } else {
+          console.log("cancel");
+        }
+      });
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" style={{ color: "red" }}>
-          install app
-        </button>
+        {isShowBtn && (
+          <button
+            type="button"
+            style={{ color: "red" }}
+            ref={buttonRef}
+            onClick={handleButtonClick}
+          >
+            install app 4
+          </button>
+        )}
       </header>
     </div>
   );
